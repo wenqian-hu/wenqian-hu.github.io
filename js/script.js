82737cb5d76c2ae95ac3bbecea49efd8f038512f
wenqian-hu <wh915@nyu.edu>
2020-10-01 06:14:35 -0400

    new design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+ScrollReveal().reveal('.introduction'), { delay: 500,reset: false };
+ScrollReveal().reveal('.works-row-1'), { delay: 500,reset: false };
+ScrollReveal().reveal('.works-row-2', { delay: 500,reset: false });
+ScrollReveal().reveal('.works-row-3', { delay: 500,reset: false });
+ScrollReveal().reveal('.content-sections', { delay: 500,reset: false });
+
+
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
